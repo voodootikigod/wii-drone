785664f36d236422b95ba5eaed606491b6fc180c
Chris Williams <chris@iterativedesigns.com>
2013-02-03 14:35:02 -0500

    Update.

--- wii-drone.js
+++ wii-drone.js
@@ -1,10 +1,15 @@
 var five = require("johnny-five"),
 	board, sensor;
+
+
 var arDrone = require('ar-drone');
 var events = require('events');
 var util = require('util');
 var history = [];
 var KONAMI = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
+
+var ANIMATIONS = ['phiM30Deg', 'phi30Deg', 'thetaM30Deg', 'theta30Deg', 'theta20degYaw200deg', 'theta20degYawM200deg', 'turnaround', 'turnaroundGodown', 'yawShake', 'yawDance', 'phiDance', 'thetaDance', 'vzDance', 'wave', 'phiThetaMixed', 'doublePhiThetaMixed', 'flipAhead', 'flipBehind', 'flipLeft', 'flipRight'];
+var ANIMATION_LENGTH = 10000;   // in ms
 function isKonami() {
 	var is = true;
 	var kl = KONAMI.length;
@@ -26,17 +31,30 @@ function WiiDrone(opts) {
 		'left': 'left',
 		'right': 'right',
 		'l': 'counterClockwise',
-		'r': 'clockwise'
+		'r': 'clockwise',
+		'select': 'wave',
+		'home': 'disableEmergency'
 		
 	};
 }
 util.inherits(WiiDrone, events.EventEmitter);
-WiiDrone.prototype.rightFlip = function() {
-	this.client.animate('flipRight', 100);
+ANIMATIONS.forEach(function (anim) {
+	WiiDrone.prototype[anim] = (function (a) {
+		return function (toggle) {
+			if (toggle) {
+				this.client.animate(a, (a.indexOf("flip") >=0 ? 100 : ANIMATION_LENGTH));
+			}
+		}
+	})(anim);
+})
+
+
+WiiDrone.prototype.clearEmergency = function () {
+	this.client.disableEmergency();
 }
-WiiDrone.prototype.leftFlip = function() {
-	this.client.animate('flipLeft', 100);
-}
+
+
+
 WiiDrone.prototype.toggleStartStop = function() {
 	if(this.started) {
 		this.client.stop();
@@ -51,7 +69,15 @@ WiiDrone.prototype.toggleStartStop = function() {
 WiiDrone.prototype.handleUp = function(err, event) {
 	var buttonId = event.target.which;
 	if(this.buttonMap.hasOwnProperty(buttonId)) {
-		this.client[this.buttonMap[buttonId]](0);
+		var target = this.buttonMap[buttonId];
+		if (this.client[target]) {
+			this.client[target](0);
+		} else if (this[target]) {
+			this[target](0);
+		} else {
+			console.log("Could not find function: "+target);
+		}
+		
 	} else {
 		switch(buttonId) {
 		case 'start':
@@ -64,30 +90,133 @@ WiiDrone.prototype.handleUp = function(err, event) {
 WiiDrone.prototype.handleDown = function(err, event) {
 	var buttonId = event.target.which;
 	if(this.buttonMap.hasOwnProperty(buttonId)) {
-		this.client[this.buttonMap[buttonId]](1);
+		var target = this.buttonMap[buttonId];
+		if (this.client[target]) {
+			this.client[target](1);
+		} else if (this[target]) {
+			this[target](1);
+		} else {
+			console.log("Could not find function: "+target);
+		}
 	}
 	this.emit('down', err, event, buttonId, this);
 }
-board = new five.Board({ debug: true});
+
+
+
+
+
+
+var wiiDrone = new WiiDrone();
+
+
+
+
+board = new five.Board();
+
+
+// Setup for bread board
+// Wire Color   =>  Meaning   =>  Arduino Pin Down
+// Yellow       =>  SCK       =>  A05
+// White        =>  GND       =>  Ground
+// Red          =>  5v        =>  5v
+// Green        =>  SDA       =>  A04
+
+
 board.on("ready", function() {
-	var wiiDrone = new WiiDrone();
-	var classicController = five.Nunchuk({ pins: ["A4", "A5"], freq: 100, device: "RVL-005" });
-	board.repl.inject({
-		sensor: classicController
-	});
+
+  // Create a new `Wii.Classic` hardware instance,
+  // specifically the RVL-005 device (classic controller).
+  var classicController = five.Wii.Classic({
+    freq: 100
+  });
+
+
+  // Nunchuk Event API
+  //
+
+  // "read" (nunchuk)
+  //
+  // Fired when the joystick detects a change in
+  // axis position.
+  //
+  // nunchuk.on( "read", function( err ) {
+
+  // });
+
+  // "change", "axischange" (joystick)
+  //
+  // Fired when the joystick detects a change in
+  // axis position.
+  //
+  classicController.joystick.left.on( "change", function( err, event ) {
+    console.log(
+      "Left joystick " + event.axis,
+      event.target[ event.axis ],
+      event.axis, event.direction
+    );
+  });
+
+  classicController.joystick.right.on( "change", function( err, event ) {
+    console.log(
+      "Right joystick " + event.axis,
+      event.target[ event.axis ],
+      event.axis, event.direction
+    );
+  });
+
+  // "down"
+  // aliases: "press", "tap", "impact", "hit"
+  //
+  // Fired when any nunchuk button is "down"
+  //
+
+  // "up"
+  // alias: "release"
+  //
+  // Fired when any nunchuk button is "up"
+  //
+
+  // "hold"
+  //
+  // Fired when any nunchuk button is in the "down" state for
+  // a specified amount of time. Defaults to 500ms
+  //
+  // To specify a custom hold time, use the "holdtime"
+  // option of the Nunchuk constructor.
+  //
+
+
 	classicController.on("up", function(err, event) {
 		console.log(event);
-		history.unshift(event.target.which);
-		history.length = KONAMI.length;
-		if(event.target.which =='zl') {
-			console.log("leftflip")
-			wiiDrone.leftFlip();
-		} else if(event.target.which =='zr') {
-			console.log("rightflip")
-			wiiDrone.rightFlip();
-		} else {
-			wiiDrone.handleUp(err, event);
-		}
+		// history.unshift(event.target.which);
+		// history.length = KONAMI.length;
+		// if(event.target.which =='zl') {
+		// 	console.log("leftflip")
+		// 	wiiDrone.leftFlip();
+		// } else if(event.target.which =='zr') {
+		// 	console.log("rightflip")
+		// 	wiiDrone.rightFlip();
+		// } else {
+		wiiDrone.handleUp(err, event);
+		// }
 	});
-	classicController.on("down", function(err, event) { wiiDrone.handleDown(err, event);	});
+	classicController.on("down", function(err, event) { 
+		console.log("down", event);
+		wiiDrone.handleDown(err, event);	
+	});
+
+  // [ "down", "up", "hold" ].forEach(function( type ) {
+
+  //   classicController.on( type, function( err, event ) {
+  //     console.log(
+  //       event.target.which + " is " + type,
+
+  //       { isUp: event.target.isUp,
+  //         isDown: event.target.isDown
+  //       }
+  //     );
+  //   });
+
 });
+
